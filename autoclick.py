86f4ca586fc098dd7e9856f1fcc1a6888a143633
Junwei Hu <jasonhu0614@outlook.com>
2022-02-10 08:19:42 +0800

    First version

---
+++ autoclick.py
@@ -0,0 +1,29 @@
+# install python 2.7
+# pip install pymouse
+# pip install pyhook, from http://www.lfd.uci.edu/~gohlke/pythonlibs/
+# pip install pyuserinput
+
+from pymouse import PyMouse
+import time
+
+m = PyMouse()
+
+# a = m.position()
+# print(a)
+
+m.move(355, 503)
+
+# a = m.position()
+# print(a)
+
+cnt = 0
+
+while True:
+    cnt += 1
+    print(cnt)
+    m.click(355, 503)
+
+    for i in range(44, 0, -1):
+        time.sleep(1)
+        print("."),
+    
